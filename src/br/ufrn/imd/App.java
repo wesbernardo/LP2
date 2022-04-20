b8f7a195295673327a99bcb40ce997f988667516
Weslley Bernardo <weslley.bernardo7@gmail.com>
2022-04-19 21:50:30 -0300

    salvar

---
+++ src/br/ufrn/imd/App.java
@@ -0,0 +1,7 @@
+package br.ufrn.imd;
+
+public class App {
+    public void imprimir() {
+        System.out.println("br.ufrn.imd");
+    }
+}
---
+++ src/br/ufrn/imd/Main.java
@@ -0,0 +1,11 @@
+package br.ufrn.imd;
+
+public class Main {
+    public static void main(String[] args) {
+        var app = new br.ufrn.imd.model.App();
+        app.imprimir();
+
+        var app1 = new App();
+        app1.imprimir();
+    }
+}
